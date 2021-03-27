9db82c73a9c4bd62da27ed039f240af41188bb1e
David Maus <david.maus@sub.uni-hamburg.de>
2021-03-27 15:13:58 +0100

    Remove unused imports

--- src/main/java/hamburg/sub/iiif/presentation/Manifest.java
+++ src/main/java/hamburg/sub/iiif/presentation/Manifest.java
@@ -33,11 +33,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.Response.ResponseBuilder;
-import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.UriBuilder;
 
 import java.net.URI;
