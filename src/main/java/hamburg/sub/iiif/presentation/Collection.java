527753ab551943546fe29d95adc9e3821eb71b1d
David Maus <david.maus@sub.uni-hamburg.de>
2021-05-08 14:20:08 +0200

    Implement collection endpoint w/ dummy data

---
+++ src/main/java/hamburg/sub/iiif/presentation/Collection.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2020,2021 by Staats- und Universitätsbibliothek Hamburg
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package hamburg.sub.iiif.presentation;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.OPTIONS;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import javax.ws.rs.core.Response;
+
+import net.jcip.annotations.ThreadSafe;
+
+/**
+ * IIIF Presentation collection API.
+ */
+@ThreadSafe
+@Path("collection")
+public final class Collection
+{
+    @OPTIONS
+    public Response getToplevelCollectionCORS ()
+    {
+        return Response.noContent().build();
+    }
+
+    @GET
+    @Produces({"application/ld+json", "application/json"})
+    public Response getToplevelCollection ()
+    {
+        return Response.ok(getClass().getResourceAsStream("/collection.json")).build();
+    }
+}
