2800d62f6785ca19e97440abb67cd651b4850e7e
David Maus <david.maus@sub.uni-hamburg.de>
2021-12-22 11:32:24 +0100

    Add stubs for named collections

--- src/main/java/hamburg/sub/iiif/presentation/Collection.java
+++ src/main/java/hamburg/sub/iiif/presentation/Collection.java
@@ -31,6 +31,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.OPTIONS;
 
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import javax.ws.rs.core.Response;
@@ -66,5 +67,21 @@ public final class Collection
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    @GET
+    @Produces({"application/ld+json", "application/json"})
+    @Path("{name}")
+    public Response getNamedCollection (@PathParam("name") final String name)
+    {
+        return Response.serverError().build();
+    }
+
+    @GET
+    @Produces({"application/ld+json", "application/json"})
+    @Path("{name}/{page}")
+    public Response getNamedCollectionPage (@PathParam("name") final String name, final int page)
+    {
+        return Response.serverError().build();
     }
 }
